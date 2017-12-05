a9273763342a620b36aaff01657bee0d2de42f89
Łukasz Ławniczak <lukasz.lawniczak@datamatica.pl>
2017-12-05 19:28:01 +0100

    scenes

--- Projekt_2/lawniczakl/scenes/scenes.js
+++ Projekt_2/lawniczakl/scenes/scenes.js
@@ -1,39 +1,60 @@
 function drawPlot() {
-	var margin = {left: 80, top: 80, right: 0, bottom: 0};
-	var svg = d3.select("svg");
-	var width = svg.attr("width") - margin.left - margin.right, 
-		height = svg.attr("height") - margin.top - margin.bottom;
+	var canvas = d3.select("svg");
+	var width = 10000, 
+		height = canvas.attr("height");
+	var scenes;
 	
-	d3.json("data.json", function(data) {
+	d3.json("scenario.json", function(data) {
+		scenes = d3.nest().key(d => d.scene)
+			.rollup(d => {return {
+				start: d3.min(d.map(v => v.start).filter(v => v !== 0)), 
+				end: d3.max(d.map(v => v.end)),
+				dialogs: d
+			}})
+			.entries(data);
+		
 		var x = d3.scaleBand()
-			.domain(data.nodes)
+			.domain(scenes.map(d => d.key))
 			.range([0, width])
 			.paddingInner(0.05);
-		var y = d3.scaleBand()
-			.domain(data.nodes)
-			.range([0, height])
-			.paddingInner(0.05);
-		var fill = d3.scaleQuantize()
-			.domain([0, d3.max(data.links.map(d => d.value))])
-			.range(["#fee5d9","#fcbba1","#fc9272","#fb6a4a","#de2d26","#a50f15"]);
+			
+		var locs = d3.select("#locations");
+		var chars = d3.select("#chars");
 		
-		var grp = svg.append("g")
-			.attr("transform", "translate("+margin.left+","+margin.top+")");
-		grp.selectAll("rect").data(data.links)
-			.enter().append("rect")
-			.attr("x", d => x(d.source))
-			.attr("y", d => y(d.target))
+		locs.selectAll("rect").data(scenes).enter()
+			.append("rect")
+			.attr("x", d => x(d.key))
+			.attr("y", 0)
 			.attr("width", x.bandwidth())
-			.attr("height", y.bandwidth())
-			.style("fill", d => fill(d.value))
-			.style("tooltip", d => d.value);
+			.attr("height", height-30)
+			.style("stroke-width", 2)
+			.style("stroke", "#000000")
+			.style("fill", "#ffffff");
 		
-		svg.append("g")
-			.attr("transform", "translate("+margin.left+","+margin.top+")")
-			.call(d3.axisLeft().scale(x));
-		
-		svg.append("g")
-			.call(d3.axisRight().scale(y))
-			.attr("transform", "translate("+margin.left+","+margin.top+")rotate(270)");
+		var sceneNo = d3.scaleThreshold()
+			.domain(scenes.map(s => s.value.end))
+			.range(d3.range(0, scenes.length));
+			
+		var axis = canvas.append("g")
+			.call(d3.axisBottom().scale(x))
+			.attr("transform", "translate(0,"+(height-20)+")");
+			
+		d3.select("#slider-time").on("input", function() {
+			console.log("update");
+			var time = parseInt(document.getElementById("slider-time").value);
+			var scene = scenes[sceneNo(time)];
+			locs.attr("transform", "translate("+(x.bandwidth()+10-x(scene.key))+",0)");
+			axis.attr("transform", "translate("+(x.bandwidth()+10-x(scene.key))+","+(height-20)+")");
+			
+			names = [...new Set(scene.value.dialogs.filter(d => d.start < time)
+				.map(d => d.name))];
+			d3.select("#chars").selectAll("text").remove().data(names)
+				.enter()
+				.append("text")
+				.attr("x", x(scenes[1].key) + 0.5*x.bandwidth())
+				.attr("y", (d, i) => 20*i)
+				.text(d => d)
+				.attr("text-anchor", "middle");
+		});
 	});
 }
