72e9aeb3fe9ce309cdb7d58773db1ca158d95a7d
Łukasz Ławniczak <lukasz.lawniczak@datamatica.pl>
2017-12-19 21:56:05 +0100

    Scene name & hero plot

---
+++ Projekt_2/lawniczakl/TWD02-Filmy/server.R
@@ -0,0 +1,146 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+  
+  scenario <- reactive({
+    read.csv("data/scenario.csv") %>% 
+      filter(start != 0, end != 0)
+  })
+  
+  timeHeroData <- reactive({
+    dtl <- 60
+    dtr <- 300
+    time <- input$sliderTime
+    if(is.null(time))
+      time <- 0
+    d1 <- scenario()
+    
+    d1 <- d1 %>% filter(start > time-dtl, end < time+dtr)
+    d1$start <- pmax(d1$start, time-dtl)
+    d1$end <- pmin(d1$end, time+dtr)
+    d1
+  })
+  
+  sceneHeroData <- reactive({
+    d1 <- scenario()
+    scenes <- input$sliderScene
+    
+    d1$scene <- d1$scene %>% as.factor %>% as.integer
+    if(!is.null(scenes)) {
+      d1 <- d1 %>% filter(scene >= scenes[1], scene <= scenes[2])
+    }
+    d1 %>% select(name, scene) %>% distinct
+  })
+  
+  
+  timeHeroPlot <- function(heroes) {
+    time <- input$sliderTime
+    if(is.null(time))
+      time <- 0
+    d1 <- timeHeroData()
+    if(!is.null(heroes)) {
+      d1 <- d1 %>% filter(name %in% heroes)
+      d1$name <- factor(d1$name, levels=heroes)
+    }
+    
+    ggplot(d1, 
+           aes(x=start, xend=end, y=name, yend=name, color=name)) +
+      geom_segment(size=6) +
+      geom_vline(xintercept=time) +
+      scale_x_time(name="time") +
+      theme(axis.title.y=element_blank(), 
+            axis.title.x=element_blank(),
+            legend.position="none")
+  }
+  
+  sceneHeroPlot <- function(heroes) {
+    d1 <- sceneHeroData()
+    if(is.null(heroes)) {
+      most_frequent <- d1 %>% group_by(name) %>%
+        summarise(n = n()) %>% arrange(desc(n)) %>% head(n=9)
+      d1 <- d1 %>% filter(name %in% most_frequent$name)
+    } else {
+      d1 <- d1 %>% filter(name %in% heroes)
+      d1$name <- factor(d1$name, levels=heroes)
+    }
+    ggplot(d1, aes(x=scene, xend=scene+1, y=name, yend=name, color=name)) +
+      geom_segment(size=6) +
+      theme(legend.position="none",
+            axis.title.x=element_blank(),
+            axis.title.y=element_blank())
+  }
+  
+  
+  output$sliderTimeUI <- renderUI({
+    data <- scenario()
+    if(input$group == "time") {
+      maxTime <- data %>% select(end) %>% as.vector %>% max
+      minVal <- as.POSIXct(0, origin="1970-01-01", tz="UTC")
+      maxVal <- as.POSIXct(maxTime, origin="1970-01-01", tz="UTC")
+      sliderInput("sliderTime", label="time", 
+                  minVal, maxVal, minVal, timeFormat="%T",
+                  timezone="UTC")
+    } else {
+      maxVal <- data %>% select(scene) %>% distinct %>% nrow
+      sliderInput("sliderScene", label="scene",
+                  1, maxVal, c(1, maxVal), step=1)
+    }
+  })
+  
+  output$sceneName <- renderText({
+    if(input$group == "time") {
+      time <- input$sliderTime
+      if(is.null(time))
+        time <- 0
+      
+      sc <- scenario() %>% 
+        filter(start <= time) %>%
+        arrange(desc(start))
+      print(sc$scene)
+      if(nrow(sc) == 0)
+        sc <- "UNKNOWN"
+      else if(sc[1, "scene"] == "")
+        sc <- "UNKNOWN"
+      else
+        sc <- sc[1, "scene"]
+      sc
+    } else {
+      scNames <- scenario()$scene %>% as.factor %>% levels
+      scNames[scNames == ""] <- "UNKNOWN"
+      scenes <- input$sliderScene
+      paste(scNames[scenes[1]], scNames[scenes[2]], sep=" ~ ")
+    }
+  })
+  
+  output$heroesUI <- renderUI({
+    if(input$heroAutoSel == "manually") {
+      if(input$group == "time")
+        names <- timeHeroData()$name %>% unique
+      else
+        names <- sceneHeroData()$name %>% unique
+      names <- c(input$heroes, names)
+      selectInput("heroes", "", multiple=TRUE, choices=names,
+                  selected=input$heroes)
+    }
+  })
+  
+  output$heroGantt <- renderPlot({
+    if(input$group == "scenes") {
+      sceneHeroPlot(input$heroes)
+    } else {
+      timeHeroPlot(input$heroes)
+    }
+  })
+})
---
+++ Projekt_2/lawniczakl/TWD02-Filmy/ui.R
@@ -0,0 +1,41 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Władca pierścieni"),
+  
+  fluidRow(
+    column(4, selectInput(inputId="group", label="Group by", 
+                choices=c("scenes", "time"))),
+    column(8, htmlOutput("sliderTimeUI"))
+  ),
+  fluidRow(
+    column(12, "scene name(s):", align="center"),
+    column(12, textOutput("sceneName"), 
+           align="center", style="margin-top:-8px;
+           margin-bottom:8px;font-size:28pt;")
+  ),
+  fluidRow(
+    sidebarLayout(
+      sidebarPanel(
+        selectInput("heroAutoSel", "Select visible heroes", 
+                    choices=c("automatically", "manually")),
+        htmlOutput("heroesUI")
+      ),
+      mainPanel(
+        plotOutput("heroGantt")
+      )
+    )
+  )
+))
